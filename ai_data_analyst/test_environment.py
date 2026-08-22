da0e9ed804a6f29c0fc8b2d199c3d1a6df4108df
Nihal <nihal01mohammed@gmail.com>
2026-08-22 14:50:08 +0530

    Day 1: project setup and environments

---
+++ ai_data_analyst/test_environment.py
@@ -0,0 +1,19 @@
+import sys
+import numpy
+import pandas
+import matplotlib
+import seaborn
+import streamlit
+
+print("=" * 45)
+print("AI DATA ANALYST")
+print("=" * 45)
+
+print("Python:", sys.version.split()[0])
+print("NumPy:", numpy.__version__)
+print("Pandas:", pandas.__version__)
+print("Matplotlib:", matplotlib.__version__)
+print("Seaborn:", seaborn.__version__)
+print("Streamlit:", streamlit.__version__)
+
+print("\n🔥 Data Analyst environment is ready!")
---
+++ cyber_threat_detection/test_environment.py
@@ -0,0 +1,21 @@
+import sys
+import numpy
+import pandas
+import sklearn
+import matplotlib
+import seaborn
+import joblib
+
+print("=" * 45)
+print("CYBER THREAT DETECTION")
+print("=" * 45)
+
+print("Python:", sys.version.split()[0])
+print("NumPy:", numpy.__version__)
+print("Pandas:", pandas.__version__)
+print("Scikit-learn:", sklearn.__version__)
+print("Matplotlib:", matplotlib.__version__)
+print("Seaborn:", seaborn.__version__)
+print("Joblib:", joblib.__version__)
+
+print("\n🔥 Cyber environment is ready!")
